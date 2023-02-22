0b264e89a12eb45ab9b3540dc62389ed94aebb0e
UserRon <nottinsonstephen@gmail.com>
2023-02-21 16:17:16 -0800

    3-islower.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,11 +2,13 @@
 
 /**
  * main - check the code
+ * followed by a new line.
+ * Prototype: void print_alphabet(void);
  * Return: Always 0.
  */
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet(void);
 	return (0);
 }
