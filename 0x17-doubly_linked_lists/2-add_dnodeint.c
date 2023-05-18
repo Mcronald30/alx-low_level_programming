462891b77fe1963f64b8265c7bf841310e0b6643
Ronald <nottinsonstephen@gmail.com>
2023-05-18 10:04:28 -0700

    Adding node to end

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_t - pointer of a doubly linked list.
+ * add_dnodeint - pointer of a doubly linked list.
  * @head: points to the first node of the list.
  * @n: value for the new node to be added.
  * Return: address of the new element or NULL if it failed.
