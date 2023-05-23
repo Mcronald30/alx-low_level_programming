6695af4de3220a1e4a9da1395b4d85b3e16131a4
Ronald <nottinsonstephen@gmail.com>
2023-05-23 11:48:53 -0700

    Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-d52e6c18e0723f5b025a75dea19ef365  gm
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/B3zaleel/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
