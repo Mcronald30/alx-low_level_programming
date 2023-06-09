7aa3c80afb62a843ccd4801fae3a9fc5cb643b49
Ronald <nottinsonstephen@gmail.com>
2023-06-09 12:31:04 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,18 +7,24 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *new_ht;
+	unsigned long int i;
+	int first = 1;
 
 	if (ht == NULL)
 		return;
 
-	new_ht = ht->shead;
 	printf("{");
-	while (new_ht != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("'%s': '%s'", new_ht->key, new_ht->value);
-		new_ht = new_ht->snext;
-		if (new_ht != NULL)
-			printf(", ");
+		new_ht = ht->array[i];
+		while (new_ht != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", new_ht->key, new_ht->value);
+			first = 0;
+			new_ht = new_ht->next;
+		}
 	}
 	printf("}\n");
 }
