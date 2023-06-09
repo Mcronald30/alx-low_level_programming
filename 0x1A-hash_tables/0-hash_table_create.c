ad6fb8f460cfbb2fbe3653d6baa23012f1d3ea85
Ronald <nottinsonstephen@gmail.com>
2023-06-09 13:49:33 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
-	unsigned long int i;
+	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
