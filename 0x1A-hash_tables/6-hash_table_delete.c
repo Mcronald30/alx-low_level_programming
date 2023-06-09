72eed8ad77ad3117b669e74cc37d1cd40f9d463c
Ronald <nottinsonstephen@gmail.com>
2023-06-09 12:43:27 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,19 +9,19 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *new_ht, *table;
 	unsigned long int i;
 
-	if (ht == NULL)
-		return;
-
 	for (i = 0; i < ht->size; i++)
 	{
-		new_ht = ht->array[i];
-		while (new_ht != NULL)
+		if (ht->array[i] != NULL)
 		{
-			table = new_ht;
-			new_ht = new_ht->next;
-			free(table->key);
-			free(table->value);
-			free(table);
+			new = ht->array[i];
+			while (new_ht != NULL)
+			{
+				table = new_ht->next;
+				free(new_ht->key);
+				free(new_ht->value);
+				free(new_ht);
+				new_ht = table;
+			}
 		}
 	}
 	free(ht->array);
