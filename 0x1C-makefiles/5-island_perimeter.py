d29b8efa31294d262aa0a1e6dc32e37e5154d9db
Ronald <nottinsonstephen@gmail.com>
2023-07-26 11:57:40 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-"""
-Class Island Perimeter
-"""
+"""Class Island Perimeter"""
 
 
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
-
-    Args:
-    grid (List[List[int]]): A rectangular grid containing 0's
-    (water zones) and 1's (land zones).
-
-    Returns:
-    int: The perimeter of the island.
-    """
-    rows = len(grid) - 1
-    cols = len(grid[0]) - 1
-    perimeter = 0
+    """Calculate the perimeter of the island described in the grid"""
+    c = 0
+    row = len(grid) - 1
+    col = len(grid[0]) - 1
 
     for i, r in enumerate(grid):
         for j, n in enumerate(r):
             if n == 1:
                 if i == 0 or grid[i - 1][j] != 1:
                     c += 1
-                    if j == 0 or grid[i][j - 1] != 1:
-                        c += 1
-                        if j == cols or grid[i][j + 1] != 1:
-                            c += 1
-                            if i == rows or grid[i + 1][j] != 1:
-                                c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == col or grid[i][j + 1] != 1:
+                    c += 1
+                if i == row or grid[i + 1][j] != 1:
+                    c += 1
     return c
